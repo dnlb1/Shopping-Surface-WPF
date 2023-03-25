ea56e179e3d4925f20e2ceca9316fa74d4c4bd32
dnlb1 <dnlb0212@gmail.com>
2023-03-25 13:50:08 +0100

    Exceptions Created

---
+++ Model/Exceptions/CannotDistributeException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Exceptions
+{
+    public class CannotDistributeException : Exception
+    {
+        public CannotDistributeException()
+        {
+        }
+
+        public CannotDistributeException(string message) : base(message)
+        {
+        }
+
+        public CannotDistributeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CannotDistributeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Model/Exceptions/NotInException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Exceptions
+{
+    public class NotInException : Exception
+    {
+        public NotInException()
+        {
+        }
+
+        public NotInException(string message) : base(message)
+        {
+        }
+
+        public NotInException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotInException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
