d08350a71e3edd25734a27e51e5f850500356a7e
dnlb1 <dnlb0212@gmail.com>
2023-03-25 13:54:55 +0100

    Add function added

--- Model/Structures/BinaryTree.cs
+++ Model/Structures/BinaryTree.cs
@@ -8,6 +8,7 @@ namespace Model.Structures
 {
     public class BinaryTree<T,N,K> where K: IComparable
     {
+        TreeElement root;
         class TreeElement //Adott termék, cikkszám alapján rendezve
         {
             public T Contant;
@@ -16,5 +17,36 @@ namespace Model.Structures
             public TreeElement bal;
             public TreeElement jobb;
         }
+
+        public void Add(T Contant, K Key, N Name)
+        {
+            _Add(ref root, Contant, Key, Name);
+        }
+        private void _Add(ref TreeElement p, T Contant, K Key, N Name)
+        {
+            if (p == null)
+            {
+                p = new TreeElement();
+                p.Contant = Contant;
+                p.ByName = Name;
+                p.Key = Key;
+            }
+            else
+            {
+                if (p.Key.CompareTo(Key) < 0)
+                {
+                    _Add(ref p.bal, Contant, Key, Name);
+                }
+                else if (p.Key.CompareTo(Key) > 0)
+                {
+                    _Add(ref p.jobb, Contant, Key, Name);
+                }
+                else
+                { 
+                    _Add(ref p.bal, Contant, Key, Name);
+                }
+            }
+        }
+
     }
 }
