4cc5f7953d8fac34ecdd833ac87e8e8f832b7ecf
dnlb1 <dnlb0212@gmail.com>
2023-03-25 13:26:33 +0100

    Interface added

---
+++ Model/Interface/ISeller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Interface
+{
+    public interface ISeller
+    {
+        string TaxNumber { get; set; }
+        string ContactPerson { get; set; }
+        int Rating { get; set; }
+
+    }
+}
