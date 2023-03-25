544f6b7ba25b15faf5983d1acef130fc2aefb3a5
dnlb1 <dnlb0212@gmail.com>
2023-03-25 13:52:05 +0100

    Binary tree with Tree element

---
+++ Model/Structures/BinaryTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Structures
+{
+    public class BinaryTree<T,N,K> where K: IComparable
+    {
+        class TreeElement //Adott termék, cikkszám alapján rendezve
+        {
+            public T Contant;
+            public K Key;
+            public N ByName; //termék neve
+            public TreeElement bal;
+            public TreeElement jobb;
+        }
+    }
+}
