30822674b9b45cf20576be94691bc10abc0e6d8f
dnlb1 <dnlb0212@gmail.com>
2023-03-25 13:27:53 +0100

    LegalPerson added

---
+++ Model/Classes Hiearchy/LegalPerson.cs
@@ -0,0 +1,16 @@
+﻿using Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Classes_Hiearchy
+{
+    public class LegalPerson : ISeller
+    {
+        public string TaxNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ContactPerson { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Rating { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
