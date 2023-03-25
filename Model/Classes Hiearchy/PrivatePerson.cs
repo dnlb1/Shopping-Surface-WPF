b4bf42959ef3508b946b65cb633fc351c887b32d
dnlb1 <dnlb0212@gmail.com>
2023-03-25 13:30:45 +0100

    PrivatePerson added

---
+++ Model/Classes Hiearchy/PrivatePerson.cs
@@ -0,0 +1,18 @@
+﻿using Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Classes_Hiearchy
+{
+    public class PrivatePerson : ISeller
+    {
+        public string TaxNumber { get; set; }
+        public string ContactPerson { get; set; }
+        public int Rating { get; set; }
+
+
+    }
+}
