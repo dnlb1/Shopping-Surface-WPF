0978ef04593f2f46fbf571ff0c52675fa5fe465e
dnlb1 <dnlb0212@gmail.com>
2023-04-07 14:56:01 +0200

    isindesign

--- Shopping Surface WPF/ViewModels/RegisterViewModels/PrivateMemberRegisterViewModel.cs
+++ Shopping Surface WPF/ViewModels/RegisterViewModels/PrivateMemberRegisterViewModel.cs
@@ -1,14 +1,24 @@
 ﻿using Model.Classes_Hiearchy;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Shopping_Surface_WPF.ViewModels.RegisterViewModels
 {
     class PrivateMemberRegisterViewModel
     {
         public PrivatePerson Person { get; set; }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
     }
 }
