63e5b6a7564c4cf96c38d7915de47c72c8879c64
dnlb1 <dnlb0212@gmail.com>
2023-04-07 13:38:17 +0200

    Logic added

---
+++ Shopping Surface WPF/Logic/IRegisterLogic.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping_Surface_WPF.Logic
+{
+    interface IRegisterLogic
+    {
+        void RegisterLegal();
+        void RegisterLimited();
+        void RegisterLTD();
+        void RegisterPrivate();
+    }
+}
---
+++ Shopping Surface WPF/Logic/RegisterLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Surface_WPF.Logic
+{
+    class RegisterLogic : IRegisterLogic
+    {
+        public void RegisterLegal()
+        {
+
+        }
+        public void RegisterLimited()
+        {
+
+        }
+        public void RegisterLTD()
+        {
+
+        }
+        public void RegisterPrivate()
+        {
+
+        }
+    }
+}
