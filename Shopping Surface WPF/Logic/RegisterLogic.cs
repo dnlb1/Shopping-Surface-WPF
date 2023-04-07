a68a1fa275d5068535dae7bdccd8efafe0e48873
dnlb1 <dnlb0212@gmail.com>
2023-04-07 14:47:52 +0200

    Added to logic register

--- Shopping Surface WPF/Logic/RegisterLogic.cs
+++ Shopping Surface WPF/Logic/RegisterLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shopping_Surface_WPF.RegisterWindows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,8 @@ namespace Shopping_Surface_WPF.Logic
         }
         public void RegisterPrivate()
         {
-
+            PrivateMemberRegister W = new PrivateMemberRegister();
+            W.ShowDialog();
         }
     }
 }
