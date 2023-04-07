ddf5b1e0f0e289f5fbaa061068d6fdc6b5836b91
dnlb1 <dnlb0212@gmail.com>
2023-04-07 14:23:27 +0200

    Private member register vm/v

---
+++ Shopping Surface WPF/RegisterWindows/PrivateMemberRegister.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Shopping_Surface_WPF.RegisterWindows
+{
+    /// <summary>
+    /// Interaction logic for PrivateMemberRegister.xaml
+    /// </summary>
+    public partial class PrivateMemberRegister : Window
+    {
+        public PrivateMemberRegister()
+        {
+            InitializeComponent();
+        }
+    }
+}
