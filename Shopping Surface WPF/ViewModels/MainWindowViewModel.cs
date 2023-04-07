48136c546c17f20fca2888a8016720188602fcdc
dnlb1 <dnlb0212@gmail.com>
2023-04-07 12:37:36 +0200

    IsInDesignMode

--- Shopping Surface WPF/ViewModels/MainWindowViewModel.cs
+++ Shopping Surface WPF/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Shopping_Surface_WPF.ViewModels
 {
     class MainWindowViewModel : ObservableRecipient
     {
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
 
     }
 }
