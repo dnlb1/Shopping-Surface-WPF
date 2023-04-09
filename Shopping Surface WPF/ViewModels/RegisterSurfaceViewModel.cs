0e7068377fd127f04fb363f1f7b738d92e5a75d1
dnlb1 <dnlb0212@gmail.com>
2023-04-09 14:46:29 +0200

    connacted limited

--- Shopping Surface WPF/ViewModels/RegisterSurfaceViewModel.cs
+++ Shopping Surface WPF/ViewModels/RegisterSurfaceViewModel.cs
@@ -56,6 +56,7 @@ namespace Shopping_Surface_WPF.ViewModels
             });
             AddLimited = new RelayCommand(() =>
             {
+                logic.RegisterLimited();
             },
             () =>
             {
