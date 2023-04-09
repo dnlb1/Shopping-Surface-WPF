f296aab16bf6a1ca369618827427d36abe69b756
dnlb1 <dnlb0212@gmail.com>
2023-04-09 16:55:11 +0200

    IsInDesignMode

--- Shopping Surface WPF/ViewModels/ButtonViewModels/SearchByNumberWindowViewModel.cs
+++ Shopping Surface WPF/ViewModels/ButtonViewModels/SearchByNumberWindowViewModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Shopping_Surface_WPF.ViewModels.ButtonViewModels
 {
-    class SearchByNumberWindowViewModel
+    class SearchByNumberWindowViewModel : ObservableRecipient
     {
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
     }
 }
